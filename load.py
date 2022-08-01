f8a996a30f6535ea02c81cffb5cdff83e67e11b5
Maryam Alam <maryamalam15@gmail.com>
2022-08-01 23:09:50 +0200

    - creating output folder only if it doesn't exist.

--- load.py
+++ load.py
@@ -1,5 +1,10 @@
+import os
+
+
 def write_df_to_csv(df, file_name, index=False):
     """
     writes the df to the given csv file.
     """
+    os.makedirs('output', exist_ok=True)
+
     df.to_csv(f'output/{file_name}.csv', index=index)
